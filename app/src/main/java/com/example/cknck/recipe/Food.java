fd5fd722d0703866af172fb7da2f0d74cf2c9de7
purplxider <jaehyunok@cau.ac.kr>
2019-06-09 20:30:55 +0900

    부족한 재료를 찾는 함수 작성

--- app/src/main/java/com/example/cknck/recipe/Food.java
+++ app/src/main/java/com/example/cknck/recipe/Food.java
@@ -1,10 +1,11 @@
 package com.example.cknck.recipe;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class Food {
 
     String foodName;
-    Ingredient ingredients[];
+    ArrayList<Ingredient> ingredients;
     URL url;
 
 
@@ -16,9 +17,23 @@ public class Food {
         this.foodName = foodName;
     }
 
-    public Food(String foodName, Ingredient ingredients[], URL url) {
+    public Food(String foodName, ArrayList<Ingredient> ingredients, URL url) {
         this.foodName = foodName;
         this.ingredients = ingredients;
         this.url = url;
+    }
+
+    ArrayList<Ingredient> findMissing(ArrayList<Ingredient> recipeIngredient) {
+        ArrayList<Ingredient> missing = recipeIngredient;
+
+        for (int i = 0; i < missing.size(); i++) {
+            for (int j = 0; j < ingredients.size(); j++) {
+                if (missing.get(i).equals(ingredients.get(j))) {
+                    missing.remove(i);
+                }
+            }
+        }
+
+        return missing;
     }
 }
